f2f275baaefd429c2f11069f977c2c1fe4b4e9f9
Michal Muransky <mike.muransky@gmail.com>
2020-11-26 13:20:37 +0100

    Add fish customs

--- playbooks/files/fish_greeting.fish
+++ playbooks/files/fish_greeting.fish
@@ -1,10 +1,10 @@
 function fish_greeting
-    set_color blue; printf "\
+    set_color cyan; printf "\
   _____ _     _           _          _ _ 
  |  ___(_)___| |__    ___| |__   ___| | |
  | |_  | / __| '_ \  / __| '_ \ / _ \ | |
  |  _| | \__ \ | | | \__ \ | | |  __/ | |
  |_|   |_|___/_| |_| |___/_| |_|\___|_|_|\n" | cat
-    fish_logo blue cyan green
+#    fish_logo blue cyan green
     set_color cyan; printf " It is "; printf (date)"\n"
 end
