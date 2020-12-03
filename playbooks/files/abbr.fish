0d7b8f9e779c19d8234f1b38bb2f14be1e44bdc2
Michal Muransky <mike.muransky@gmail.com>
2020-12-03 19:16:43 +0100

    Add abbr.fish

--- playbooks/files/abbr.fish
+++ playbooks/files/abbr.fish
@@ -2,7 +2,7 @@ if status --is-interactive
  abbr -a -g a argocd
  abbr -a -g ap ansible-playbook
  abbr -a -g gc "git commit"
- abbr -a -g gps "git push"
+ abbr -a -g gph "git push"
  abbr -a -g gpl "git pull"
  abbr -a -g k kubectl
  abbr -a -g d docker
