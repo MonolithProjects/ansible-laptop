92af12e0e026b0519af556f0f058dac7d0d32fc4
Mike <mike.muransky@gmail.com>
2024-12-31 12:16:11 +0100

    update prompt

--- playbooks/templates/config.fish
+++ playbooks/templates/config.fish
@@ -1,4 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    oh-my-posh init fish --config ~/.config/fish/.my_prompt.json | source
+    tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
 end
